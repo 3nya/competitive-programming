ec568c3529b3c0bcc27c5e5302ab825e104350fd
Leaves333 <canchenli@gmail.com>
2024-11-13 19:03:00 -0600

    edit template

--- content/template.cpp
+++ content/template.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
 typedef long long ll;
 typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 typedef vector<int> vi;
+typedef vector<ll> vll;
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
